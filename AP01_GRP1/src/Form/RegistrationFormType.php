cb575d619fec9567bd606215f7756e1530f517c0
Vulqy (Elvin) <elvin.chauvel@stadjutor.com>
2023-09-23 22:09:06 +0200

    Register Interface

---
+++ AP01_GRP1/src/Form/RegistrationFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomUtil')
+            ->add('prenomUtil')
+            ->add('emailUtil')
+            ->add('loginUtil')
+            ->add('mdpUtil')
+            ->add('droitUtil')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
+}
