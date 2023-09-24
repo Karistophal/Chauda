461a58833780a27f0323ae3ec0b60987d7599cab
Vulqy (Elvin) <elvin.chauvel@stadjutor.com>
2023-09-24 11:46:19 +0200

    Login Fonctionnel
    Le login fonctionne parfaitement le design est aussi terminé

--- AP01_GRP1/src/Controller/RegistrationController.php
+++ AP01_GRP1/src/Controller/RegistrationController.php
@@ -10,12 +10,32 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register", name="app_register")
      */
+
+     private $security;
+
+     public function __construct(Security $security)
+     {
+         $this->security = $security;
+     }
+
+     public function index(Security $security): Response
+     {
+         // Si l'utilisateur est déjà connecté, redirigez-le vers la page de présentation
+         if ($security->getUser()) {
+             return $this->redirectToRoute('presentation');
+         }
+     
+         return $this->redirectToRoute('app_register');
+     }
+
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $user = new Utilisateur();
@@ -24,6 +44,7 @@ class RegistrationController extends AbstractController
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
+        
         if ($form->isSubmitted() && $form->isValid()) {
             // Encode the plain password
             $user->setMdpUtil(
